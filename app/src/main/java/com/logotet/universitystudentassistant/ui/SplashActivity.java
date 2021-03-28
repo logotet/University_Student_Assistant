1096b5c871f51623783b495e2169fa06614e6773
logotet <logotetsp@gmail.com>
2021-03-28 19:19:33 +0300

    SplashActivity refactored

--- app/src/main/java/com/logotet/universitystudentassistant/ui/SplashActivity.java
+++ app/src/main/java/com/logotet/universitystudentassistant/ui/SplashActivity.java
@@ -1,37 +1,26 @@
 package com.logotet.universitystudentassistant.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.WindowManager;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.logotet.universitystudentassistant.R;
 import com.logotet.universitystudentassistant.data.Repository;
 import com.logotet.universitystudentassistant.data.entities.UniversityEntityPrep;
-import com.logotet.universitystudentassistant.data.local.AppDatabase;
-import com.opencsv.CSVReader;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
 
 public class SplashActivity extends AppCompatActivity {
 
     private Repository repository;
-    private boolean isPrepDbEmpty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,49 +31,39 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         readData();
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-            }
-        }, 2000);
+        new Handler().postDelayed(() ->
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class)),
+                2000);
 
     }
 
     private void readData() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-
-        InputStream inputStream = getResources().openRawResource(R.raw.all_university_database_table);
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(inputStream, StandardCharsets.UTF_8)
-        );
-
-        String line = null;
-        try {
-            bufferedReader.readLine();
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] tokens = line.split(",");
-                UniversityEntityPrep entityPrep = new UniversityEntityPrep(tokens[0]);
-                entityPrep.setAddress(tokens[1]);
-                entityPrep.setCity(tokens[2]);
-                entityPrep.setState(tokens[3]);
-                entityPrep.setWebPage(tokens[tokens.length - 1]);
-                Log.i("Entity", entityPrep.getName() + " "
-                        + entityPrep.getAddress() + ", "
-                        + entityPrep.getCity() + " "
-                        + entityPrep.getState() + " "
-                        + "web page " + entityPrep.getWebPage());
-                repository.insertPrepUniversity(entityPrep);
-            }
-
-        } catch (IOException e) {
-            Log.wtf("Splash Activity", "Error reading file!!! " + line, e);
-            e.printStackTrace();
+        new Thread(() -> {
+    InputStream inputStream = getResources().openRawResource(R.raw.all_university_database_table);
+    BufferedReader bufferedReader = new BufferedReader(
+            new InputStreamReader(inputStream, StandardCharsets.UTF_8)
+    );
+    String line = null;
+    try {
+        bufferedReader.readLine();
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] tokens = line.split(",");
+            UniversityEntityPrep entityPrep = new UniversityEntityPrep(tokens[0]);
+            entityPrep.setAddress(tokens[1]);
+            entityPrep.setCity(tokens[2]);
+            entityPrep.setState(tokens[3]);
+            entityPrep.setWebPage(tokens[tokens.length - 1]);
+            Log.i("Entity", entityPrep.getName() + " "
+                    + entityPrep.getAddress() + ", "
+                    + entityPrep.getCity() + " "
+                    + entityPrep.getState() + " "
+                    + "web page " + entityPrep.getWebPage());
+            repository.insertPrepUniversity(entityPrep);
         }
-            }
+    } catch (IOException e) {
+        Log.wtf("Splash Activity", "Error reading file!!! " + line, e);
+        e.printStackTrace();
+    }
         });
     }
 }
